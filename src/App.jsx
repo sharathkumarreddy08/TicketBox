7e8cc2b37ca82679626a50b8999e08cfaf384dc6
sharathkumarreddy08 <sharathkumarreddy357@gmail.com>
2025-09-17 11:51:36 +0530

    issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { Button } from 'react-bootstrap';
 
 
 function App() {
-  const [user,setUser] =useState('');
+  const [user, setUser] = useState('');
   const navigate = useNavigate();
 
   useEffect(()=>{
