f2c414a788d9b26fcf7ef7b3310342f2a76da551
sharathkumarreddy08 <sharathkumarreddy357@gmail.com>
2025-09-17 10:59:05 +0530

    updated signup

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -40,7 +40,7 @@ const handleSubmit =() =>{
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="formBasicPassword1">
         <Form.Label>confirm Password</Form.Label>
         <Form.Control type="password" placeholder=" confirm Password" />
       </Form.Group>
